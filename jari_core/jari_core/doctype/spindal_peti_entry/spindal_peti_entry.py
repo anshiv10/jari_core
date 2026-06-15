41f9339e24f98d1976f4da3a5e042c46c1d814ca
Anshiv <dixitanshiv123@gmail.com>
2026-06-15 11:18:09 +0000

    Describe what you have changed

--- jari_core/jari_core/doctype/spindal_peti_entry/spindal_peti_entry.py
+++ jari_core/jari_core/doctype/spindal_peti_entry/spindal_peti_entry.py
@@ -1,6 +1,6 @@
 import frappe
 from frappe.model.document import Document
-from frappe.utils import flt, today
+from frappe.utils import flt
 
 
 class SpindalPetiEntry(Document):
@@ -8,6 +8,7 @@ class SpindalPetiEntry(Document):
     def validate(self):
         self.pull_spindal_issue_details()
         self.calculate_net_weight()
+        self.validate_weights()
 
     def on_submit(self):
         self.status = "Received"
@@ -19,11 +20,21 @@ class SpindalPetiEntry(Document):
         issue = frappe.get_doc("Spindal Issue", self.spindal_issue)
 
         self.company = issue.company
-        self.batch_no = issue.batch_no
+        self.batch_no = issue.active_batch_no
         self.quality_code = issue.quality_code
+
+        if hasattr(self, "operator") and not self.operator:
+            self.operator = issue.operator
 
     def calculate_net_weight(self):
         self.net_weight = flt(self.gross_weight) - flt(self.baad_weight)
 
-        if self.net_weight < 0:
-            frappe.throw("Net Weight cannot be negative.")
+    def validate_weights(self):
+        if flt(self.gross_weight) <= 0:
+            frappe.throw("Gross Weight must be greater than zero.")
+
+        if flt(self.baad_weight) < 0:
+            frappe.throw("Baad Weight cannot be negative.")
+
+        if flt(self.net_weight) <= 0:
+            frappe.throw("Net Weight must be greater than zero.")
