f7003fb42814bd2151b811d4ce7ffefe8e20871b
Anshiv <dixitanshiv123@gmail.com>
2026-06-24 19:59:50 +0000

    Describe what you have changed

--- jari_core/jari_core/doctype/gilit_issue/gilit_issue.py
+++ jari_core/jari_core/doctype/gilit_issue/gilit_issue.py
@@ -94,7 +94,7 @@ class GilitIssue(Document):
             row.quality_code = peti.quality_code
             row.khata_no = peti.khata_no
             row.product = self.get_kasab_product()
-            row.uom = "GM"
+            row.uom = "KG"
             row.gross_weight = flt(peti.gross_weight)
             row.baad_weight = flt(peti.baad_weight)
             row.net_weight = flt(peti.net_weight)
