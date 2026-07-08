a8f2e4cbe6d787cbb3bc252712bd36065c4b8c19
Anshiv <dixitanshiv123@gmail.com>
2026-07-08 16:31:02 +0000

    Remove accidental backup files from app

--- jari_core/jari_core/doctype_backup_spindal_payout_2026-07-08_1626/metal_recovery_entry/metal_recovery_entry.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, anSHIV and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Metal Recovery Entry", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- jari_core/jari_core/doctype_backup_spindal_payout_2026-07-08_1626/spindal_issue_item/spindal_issue_item.py
+++
@@ -1,5 +0,0 @@
-from frappe.model.document import Document
-
-
-class SpindalIssueItem(Document):
-    pass
