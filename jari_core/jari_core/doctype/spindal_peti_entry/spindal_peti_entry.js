55361b5f0a7a48720141ae71ef76d96dff3d4e97
Anshiv <dixitanshiv123@gmail.com>
2026-07-09 11:34:42 +0000

    Describe what you have changed

--- jari_core/jari_core/doctype/spindal_peti_entry/spindal_peti_entry.js
+++ jari_core/jari_core/doctype/spindal_peti_entry/spindal_peti_entry.js
@@ -2,7 +2,9 @@ frappe.ui.form.on('Spindal Peti Entry', {
     refresh(frm) {
         frm.set_query('spindal_issue', function() {
             return {
-                query: 'jari_core.jari_core.doctype.spindal_receive.spindal_receive.spindal_issue_for_peti_query'
+                filters: {
+                    docstatus: ['!=', 1]
+                }
             };
         });
     }
