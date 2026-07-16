14c99b57a63ac7ec3029c27cc6e13d33bdce846f
Anshiv <dixitanshiv123@gmail.com>
2026-07-16 11:09:52 +0000

    Describe what you have changed

--- jari_core/jari_core/doctype/pavtha_receive/pavtha_receive.js
+++ jari_core/jari_core/doctype/pavtha_receive/pavtha_receive.js
@@ -33,8 +33,29 @@ frappe.ui.form.on('Pavtha Receive', {
                 frm.set_value('process_master', issue.process_master);
                 frm.set_value('quality_code', issue.quality_code);
                 frm.set_value('outsourcer', issue.outsourcer);
-                frm.set_value('rate_per_kg', issue.rate_per_kg);
                 frm.set_value('total_input_weight', issue.total_issue_weight);
+
+                if (issue.outsourcer) {
+                    frappe.db.get_value(
+                        'Jobworker Master',
+                        issue.outsourcer,
+                        ['rate_per_kg', 'standard_loss_percent']
+                    ).then(r => {
+                        const values = r.message || {};
+
+                        frm.set_value(
+                            'rate_per_kg',
+                            flt(values.rate_per_kg)
+                        );
+
+                        frm.set_value(
+                            'loss_standard_percent',
+                            flt(values.standard_loss_percent)
+                        );
+                    });
+                } else {
+                    frm.set_value('rate_per_kg', 0);
+                }
 
                 frappe.call({
                     method: 'frappe.client.get',
@@ -74,3 +95,82 @@ frappe.ui.form.on('Pavtha Receive', {
         });
     }
 });
+
+
+frappe.ui.form.on('Pavtha Output Item', {
+    weight(frm) {
+        calculate_pavtha_preview(frm);
+    },
+
+    output_items_remove(frm) {
+        calculate_pavtha_preview(frm);
+    }
+});
+
+
+frappe.ui.form.on('Pavtha Waste Item', {
+    weight(frm) {
+        calculate_pavtha_preview(frm);
+    },
+
+    waste_items_remove(frm) {
+        calculate_pavtha_preview(frm);
+    }
+});
+
+
+function calculate_pavtha_preview(frm) {
+    const input = flt(frm.doc.total_input_weight);
+
+    const output = (frm.doc.output_items || []).reduce(
+        (total, row) => total + flt(row.weight),
+        0
+    );
+
+    const waste = (frm.doc.waste_items || []).reduce(
+        (total, row) => total + flt(row.weight),
+        0
+    );
+
+    const loss = input - output - waste;
+    const lossPercent = input ? (loss / input) * 100 : 0;
+    const wastePercent = input ? (waste / input) * 100 : 0;
+
+    const rate = flt(frm.doc.rate_per_kg);
+    const standardLoss = flt(frm.doc.loss_standard_percent);
+
+    const basePayout = input * rate;
+    const variancePercent = lossPercent - standardLoss;
+
+    let bonus = 0;
+    let deduction = 0;
+
+    if (variancePercent > 0) {
+        const excessWeight = input * variancePercent / 100;
+        deduction = excessWeight * rate;
+    } else if (variancePercent < 0) {
+        const savedWeight = input * Math.abs(variancePercent) / 100;
+        bonus = savedWeight * rate;
+    }
+
+    const suggestion = basePayout + bonus - deduction;
+
+    frm.set_value('total_output_weight', output);
+    frm.set_value('total_waste_weight', waste);
+    frm.set_value('loss_weight', loss);
+    frm.set_value('loss_percent', lossPercent);
+    frm.set_value('waste_percent', wastePercent);
+    frm.set_value('base_payout', basePayout);
+    frm.set_value('bonus_amount', bonus);
+    frm.set_value('deduction_amount', deduction);
+    frm.set_value('payout_suggestion', suggestion);
+
+    if (!flt(frm.doc.payout_given)) {
+        frm.set_value('payout_given', suggestion);
+    }
+
+    frm.set_value(
+        'loss_status',
+        lossPercent > standardLoss ? 'Excess Loss' : 'OK'
+    );
+}
