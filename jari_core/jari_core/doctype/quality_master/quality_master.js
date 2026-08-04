d91188df7f5d6c9131c1e273a35621231d07a7e4
Anshiv <dixitanshiv123@gmail.com>
2026-08-04 13:28:50 +0000

    Describe what you have changed

--- jari_core/jari_core/doctype/quality_master/quality_master.js
+++ jari_core/jari_core/doctype/quality_master/quality_master.js
@@ -1,8 +1,55 @@
 // Copyright (c) 2026, anSHIV and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Quality Master", {
-// 	refresh(frm) {
+frappe.ui.form.on("Quality Master", {
+    setup(frm) {
+        frm.set_query("department", function () {
+            const filters = {
+                active: 1
+            };
 
-// 	},
-// });
+            if (frm.doc.flow_type) {
+                filters.flow_type = frm.doc.flow_type;
+            }
+
+            return {
+                filters: filters
+            };
+        });
+    },
+
+    flow_type(frm) {
+        if (!frm.doc.department) {
+            return;
+        }
+
+        frappe.db.get_value(
+            "Department Master",
+            frm.doc.department,
+            ["flow_type", "active"]
+        ).then((r) => {
+            const department = r.message;
+
+            if (!department) {
+                frm.set_value("department", "");
+                return;
+            }
+
+            if (
+                cint(department.active) !== 1 ||
+                (
+                    frm.doc.flow_type &&
+                    department.flow_type !== frm.doc.flow_type
+                )
+            ) {
+                frm.set_value("department", "");
+                frappe.show_alert({
+                    message: __(
+                        "Department was cleared because it does not match the selected Flow Type or is inactive."
+                    ),
+                    indicator: "orange"
+                });
+            }
+        });
+    }
+});
--- jari_core/jari_core/doctype/quality_master/quality_master.py
+++ jari_core/jari_core/doctype/quality_master/quality_master.py
@@ -1,9 +1,53 @@
 # Copyright (c) 2026, anSHIV and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
+from frappe.utils import cint
 
 
 class QualityMaster(Document):
-	pass
+
+    def validate(self):
+        self.validate_department()
+
+    def validate_department(self):
+        if not self.department:
+            frappe.throw(_("Department is required."))
+
+        department = frappe.db.get_value(
+            "Department Master",
+            self.department,
+            ["department_name", "flow_type", "active"],
+            as_dict=True,
+        )
+
+        if not department:
+            frappe.throw(
+                _("Department Master {0} does not exist.").format(
+                    frappe.bold(self.department)
+                )
+            )
+
+        if cint(department.active) != 1:
+            frappe.throw(
+                _("Department {0} is inactive.").format(
+                    frappe.bold(department.department_name)
+                )
+            )
+
+        if (
+            self.flow_type
+            and department.flow_type
+            and self.flow_type != department.flow_type
+        ):
+            frappe.throw(
+                _(
+                    "Quality Flow Type {0} does not match Department "
+                    "Flow Type {1}."
+                ).format(
+                    frappe.bold(self.flow_type),
+                    frappe.bold(department.flow_type),
+                )
+            )
